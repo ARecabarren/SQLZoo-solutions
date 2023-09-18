2bd7a7e8bfd2035544f638f3516bc74e2fcb372f
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-09-18 14:43:50 -0300

    Solve hard exercises

--- 7_more_join.sql
+++ 7_more_join.sql
@@ -67,5 +67,51 @@ JOIN actor ON actor.id = casting.actorid
 WHERE yr = 1962 AND casting.ord = 1
 
 -- 11.
--- Which were the busiest years for 'John Travolta', show the year and the number of movies he made each year for any year in 
+-- Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in 
 -- which he made more than 2 movies.
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
+
+-- 12.
+-- List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+SELECT title, name AS leading_actor
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+--Check if movie is in the list of movies where Julie Andrews played in
+WHERE casting.movieid IN (
+    SELECT movieid FROM casting
+    WHERE actorid IN ( --Check if Julie Andrews is in the list of actors for the movie
+        SELECT id FROM actor
+        WHERE name='Julie Andrews')) 
+        AND ord = 1 --
+
+-- 13.
+-- Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+SELECT name
+FROM actor JOIN casting
+ON actor.id = casting.actorid
+WHERE ord = 1
+GROUP BY name
+HAVING COUNT(ord) >=15
+
+-- 14.
+-- List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+SELECT title, COUNT(actorid)
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+WHERE yr = 1978
+GROUP BY title
+ORDER by COUNT(actorid) DESC, movie.title
+
+-- 15.
+-- List all the people who have worked with 'Art Garfunkel'.
+SELECT name FROM casting
+JOIN actor ON casting.actorid = actor.id
+WHERE casting.movieid IN (SELECT movieid FROM casting
+WHERE actorid = (SELECT id FROM actor
+WHERE name = 'Art Garfunkel')) AND actor.name != 'Art Garfunkel'
