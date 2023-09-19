ca7fb4ce185fb93d7178919f0a71baebd8b4f980
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-09-19 11:03:22 -0300

    Add first seven solutions

--- 8_using_null.sql
+++ 8_using_null.sql
@@ -7,4 +7,47 @@
 -- NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
 -- 1.
 -- List the teachers who have NULL for their department.
--- Why we cannot use =
+SELECT name FROM teacher
+WHERE teacher.dept IS NULL
+
+-- 2.
+-- INNER JOIN misses the teachers with no department and the departments with no teacher.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- 3.
+-- Use a different JOIN so that all teachers are listed.
+SELECT teacher.name, dept.name
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id
+
+-- 4.
+-- Use a different JOIN so that all departments are listed.
+SELECT teacher.name, dept.name
+FROM teacher
+RIGHT JOIN dept
+ON teacher.dept = dept.id
+
+-- 5.
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. 
+-- Show teacher name and mobile number or '07986 444 2266'
+SELECT teacher.name, COALESCE(teacher.mobile,'07986 444 2266')
+FROM teacher
+
+-- 6.
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name.
+-- Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name,'None')
+FROM teacher
+LEFT JOIN dept ON teacher.dept = dept.id
+
+-- 7.
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(teacher.name), COUNT(COALESCE(teacher.mobile,NULL))
+FROM teacher
+
+-- 8.
+-- Use COUNT and GROUP BY dept.name to show each department and the number of staff.
+-- Use a RIGHT JOIN to ensure that the Engineering department is listed.
