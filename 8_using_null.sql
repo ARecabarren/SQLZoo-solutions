2d3a3d7142f9fcb8b79c43717ada6257c2f8135d
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-09-19 10:49:04 -0300

    Add file to hold exercises

---
+++ 8_using_null.sql
@@ -0,0 +1,10 @@
+-- Using Null
+-- About database
+-- Teachers and Departments
+-- The school includes many departments. Most teachers work exclusively for a single department. 
+-- Some teachers have no department.
+
+-- NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+-- 1.
+-- List the teachers who have NULL for their department.
+-- Why we cannot use =
